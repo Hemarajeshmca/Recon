b1af6cdb6037d6b3f84808c9e1f942fa24fdda33
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2023-11-27 10:58:00 +0530

    rule changes

---
+++ Recon_proto/Controllers/ReconversionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recon_proto.Controllers
+{
+	public class ReconversionController : Controller
+	{
+		private IConfiguration _configuration;
+		public ReconversionController(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		string urlstring = "";
+		public IActionResult ReconversionList()
+		{
+			return View();
+		}
+	}
+}
