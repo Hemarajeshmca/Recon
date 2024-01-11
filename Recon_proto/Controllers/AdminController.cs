585a9cb8ca1031f587d1dd73ec830bad9611d47f
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-01-11 15:04:53 +0530

    still changes

--- Recon_proto/Controllers/AdminController.cs
+++ Recon_proto/Controllers/AdminController.cs
@@ -83,6 +83,51 @@ namespace Recon_proto.Controllers
 			public String? out_msg { get; set; }
 			public Int16? out_result { get; set; }
 		}
-#endregion
+		#endregion
+
+		#region list
+		[HttpPost]
+		public JsonResult userlistfetch([FromBody] usermodel context)
+		{
+			urlstring = _configuration.GetSection("Appsettings")["apiurl"].ToString();
+			DataTable result = new DataTable();			
+			string post_data = "";
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					string Urlcon = "UserManagement/";
+					client.BaseAddress = new Uri(urlstring + Urlcon);
+					client.DefaultRequestHeaders.Accept.Clear();
+					client.Timeout = Timeout.InfiniteTimeSpan;
+					client.DefaultRequestHeaders.Add("user_code", HttpContext.Session.GetString("user_code"));
+					client.DefaultRequestHeaders.Add("lang_code", HttpContext.Session.GetString("lang_code"));
+					client.DefaultRequestHeaders.Add("role_code", HttpContext.Session.GetString("role_code"));
+					client.DefaultRequestHeaders.Add("ipaddress", HttpContext.Session.GetString("ipAddress"));
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					HttpContent content = new StringContent(JsonConvert.SerializeObject(context), UTF8Encoding.UTF8, "application/json");
+					var response = client.PostAsync("userlist", content).Result;
+					Stream data = response.Content.ReadAsStreamAsync().Result;
+					StreamReader reader = new StreamReader(data);
+					post_data = reader.ReadToEnd();
+					string d2 = JsonConvert.DeserializeObject<string>(post_data);					
+					return Json(d2);
+				}
+			}
+			catch (Exception ex)
+			{
+				CommonController objcom = new CommonController(_configuration);
+				objcom.errorlog(ex.Message, "userlistfetch");
+				return Json(ex.Message);
+			}
+		}
+		public class usermodel
+		{
+			public string user_code { get; set; }
+			public string role_code { get; set; }
+			public string lang_code { get; set; }
+		}
+		#endregion
 	}
+
 }
