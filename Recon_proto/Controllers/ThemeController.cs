44de9dc031f77c5ce0398b629e6e5afcdca6769c
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-04-30 17:36:48 +0530

    new changes

--- Recon_proto/Controllers/ThemeController.cs
+++ Recon_proto/Controllers/ThemeController.cs
@@ -120,6 +120,7 @@ namespace Recon_proto.Controllers
 			public string theme_type { get; set; }
 			public string source_dataset { get; set; }
 			public string comparison_dataset { get; set; }
+			public string inactive_reason { get; set; }
 		}
 		#endregion
 
