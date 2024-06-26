32f0d04d12cb4c2c3b175622b96ed4eb65c74165
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-06-26 15:00:50 +0530

    rule chnages front end

--- Recon_proto/Controllers/DatasetfileController.cs
+++ Recon_proto/Controllers/DatasetfileController.cs
@@ -289,13 +289,54 @@ namespace Recon_proto.Controllers
 				objcom.errorlog(ex.Message, "datasetAgainstRecon");
 				return ex.Message;
 			}
-
-
 		}
 
 		public class datasetAgainstReconModel
 		{
 			public string? in_recon_code { get; set; }
+		}
+		#endregion
+
+		#region datasethistory
+		public string datasethistory([FromBody] datasethistoryModel context)
+		{
+			urlstring = _configuration.GetSection("Appsettings")["apiurl"].ToString();
+			DataTable result = new DataTable();
+			string post_data = "";
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					string Urlcon = "Dataset/";
+					client.BaseAddress = new Uri(urlstring + Urlcon);
+					client.DefaultRequestHeaders.Accept.Clear();
+					client.Timeout = Timeout.InfiniteTimeSpan;
+					client.DefaultRequestHeaders.Add("user_code", _configuration.GetSection("AppSettings")["user_code"].ToString());
+					client.DefaultRequestHeaders.Add("lang_code", _configuration.GetSection("AppSettings")["lang_code"].ToString());
+					client.DefaultRequestHeaders.Add("role_code", _configuration.GetSection("AppSettings")["role_code"].ToString());
+					client.DefaultRequestHeaders.Add("ipaddress", _configuration.GetSection("AppSettings")["ipaddress"].ToString());
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					HttpContent content = new StringContent(JsonConvert.SerializeObject(context), UTF8Encoding.UTF8, "application/json");
+					var response = client.PostAsync("datasethistory", content).Result;
+					Stream data = response.Content.ReadAsStreamAsync().Result;
+					StreamReader reader = new StreamReader(data);
+					post_data = reader.ReadToEnd();
+					string d2 = JsonConvert.DeserializeObject<string>(post_data);
+					result = JsonConvert.DeserializeObject<DataTable>(d2);
+					return d2;
+				}
+			}
+			catch (Exception ex)
+			{
+				CommonController objcom = new CommonController(_configuration);
+				objcom.errorlog(ex.Message, "datasethistory");
+				return ex.Message;
+			}
+		}
+
+		public class datasethistoryModel
+		{
+			public string? in_dataset_code { get; set; }
 		}
 		#endregion
 	}
