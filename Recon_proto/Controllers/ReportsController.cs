685008c99bc4bcb6de9fa88454361c8d0efaaef7
Hema Jayaraman <hema@flexicodeindia.com>
2024-07-02 15:14:06 +0530

    Hema front end changes

--- Recon_proto/Controllers/ReportsController.cs
+++ Recon_proto/Controllers/ReportsController.cs
@@ -2401,7 +2401,73 @@ namespace Recon_proto.Controllers
             }
         }
 
+		#region generatedynamicreport_typeC
 
-    }
+
+		[HttpPost]
+		public JsonResult generatedynamicreport_typeC([FromBody] generatedynamicReport_typeCmodel context)
+		{
+			urlstring = _configuration.GetSection("Appsettings")["apiurl"].ToString();
+			DataTable result = new DataTable();
+			string post_data = "";
+			try
+			{
+				using (var client = new HttpClient())
+				{
+					string Urlcon = "Report/";
+					client.BaseAddress = new Uri(urlstring + Urlcon);
+					//client.BaseAddress = new Uri("https://localhost:44348/api/Report/");
+					client.DefaultRequestHeaders.Accept.Clear();
+					client.Timeout = Timeout.InfiniteTimeSpan;
+					client.DefaultRequestHeaders.Add("user_code", _configuration.GetSection("AppSettings")["user_code"].ToString());
+					client.DefaultRequestHeaders.Add("lang_code", _configuration.GetSection("AppSettings")["lang_code"].ToString());
+					client.DefaultRequestHeaders.Add("role_code", _configuration.GetSection("AppSettings")["role_code"].ToString());
+					client.DefaultRequestHeaders.Add("ipaddress", _configuration.GetSection("AppSettings")["ipaddress"].ToString());
+					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+					HttpContent content = new StringContent(JsonConvert.SerializeObject(context), UTF8Encoding.UTF8, "application/json");
+					try
+					{
+						var response = client.PostAsync("generatedynamicReport_typeC", content).Result;
+						Stream data = response.Content.ReadAsStreamAsync().Result;
+						StreamReader reader = new StreamReader(data);
+						post_data = reader.ReadToEnd();
+						string d2 = JsonConvert.DeserializeObject<string>(post_data);
+						return Json(d2);
+					}
+					catch (HttpRequestException ex)
+					{
+						return Json(ex);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				CommonController objcom = new CommonController(_configuration);
+				objcom.errorlog(ex.Message, "generatedynamicreport");
+				return Json(ex.Message);
+			}
+		}
+
+		public class generatedynamicReport_typeCmodel
+		{
+
+			public String? in_reporttemplate_code { get; set; }
+			public String? in_recon_code { get; set; }
+			public String? in_report_code { get; set; }
+			public String? in_report_param { get; set; }
+			public String? in_report_condition { get; set; }
+			public String? in_ip_addr { get; set; }
+			public Boolean? in_outputfile_flag { get; set; }
+			public String? in_outputfile_type { get; set; }
+			public string? in_user_code { get; set; }
+			public string? file_name { get; set; }
+			public string? in_report_name { get; set; }
+
+		}
+
+		#endregion
+
+
+	}
 
 }
