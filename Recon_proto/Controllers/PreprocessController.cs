be23ea10e0db4f5a4f4497d6d41530da295d2555
vinoth prasath <vinothprasath.s@flexicodeindia.com>
2024-08-04 16:11:42 +0530

    front end changes

--- Recon_proto/Controllers/PreprocessController.cs
+++ Recon_proto/Controllers/PreprocessController.cs
@@ -121,6 +121,7 @@ namespace Recon_proto.Controllers
 			public string? preprocess_order { get; set; }
 			public string? lookup_dataset_code { get; set; }
 			public string? lookup_return_field { get; set; }
+			public string? lookup_multi_return_flag { get; set; }
 			public string? in_returnflag { get; set; }
 			public string? active_status { get; set; }
 			public string? in_action { get; set; }
@@ -494,6 +495,57 @@ namespace Recon_proto.Controllers
 				post_data = reader.ReadToEnd();
 				d2 = JsonConvert.DeserializeObject<string>(post_data);
 				return Json(d2);
+			}
+		}
+		#endregion
+
+		#region lookup field
+		public class lookupfieldmodel
+		{
+			public int? in_preprocesslookup_gid { get; set; }
+			public string? in_preprocess_code { get; set; }
+			public Double in_lookup_seqno { get; set; }
+			public string? in_lookup_return_field { get; set; }
+			public string? in_set_recon_field { get; set; }
+			public string? in_active_status { get; set; }
+			public string? in_action { get; set; }
+			public string? in_user_code { get; set; }
+			public string? out_msg { get; set; }
+			public string? out_result { get; set; }
+		}
+		[HttpPost]
+		public JsonResult lookupfieldsave([FromBody] lookupfieldmodel context)
+		{
+			urlstring = _configuration.GetSection("Appsettings")["apiurl"].ToString();
+			lookupfieldmodel objList = new lookupfieldmodel();
+			DataTable result = new DataTable();
+			string post_data = "";
+			string d2 = "";
+			using (var client = new HttpClient())
+			{
+				string Urlcon = "Preprocess/";
+				client.BaseAddress = new Uri(urlstring + Urlcon);
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.Timeout = Timeout.InfiniteTimeSpan;
+				client.DefaultRequestHeaders.Add("user_code", context.in_user_code);
+				client.DefaultRequestHeaders.Add("lang_code", _configuration.GetSection("AppSettings")["lang_code"].ToString());
+				client.DefaultRequestHeaders.Add("role_code", _configuration.GetSection("AppSettings")["role_code"].ToString());
+				client.DefaultRequestHeaders.Add("ipaddress", _configuration.GetSection("AppSettings")["ipaddress"].ToString());
+				client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+				HttpContent content = new StringContent(JsonConvert.SerializeObject(context), UTF8Encoding.UTF8, "application/json");
+				var response = client.PostAsync("lookupfieldsave", content).Result;
+				Stream data = response.Content.ReadAsStreamAsync().Result;
+				StreamReader reader = new StreamReader(data);
+				post_data = reader.ReadToEnd();
+				d2 = JsonConvert.DeserializeObject<string>(post_data);
+				result = JsonConvert.DeserializeObject<DataTable>(d2);
+				for (int i = 0; i < result.Rows.Count; i++)
+				{
+					objList.in_preprocesslookup_gid = Convert.ToInt32(result.Rows[i]["in_preprocesslookup_gid"]);
+					objList.out_msg = result.Rows[i]["out_msg"].ToString();
+					objList.out_result = result.Rows[i]["out_result"].ToString();
+				}
+				return Json(objList);
 			}
 		}
 		#endregion
