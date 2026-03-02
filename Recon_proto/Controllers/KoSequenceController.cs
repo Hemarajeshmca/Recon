53ccd083497be67a19a5df64c42621b458fd0ce2
vinothprasath s <vinothprasath.s@flexicodeindia.com>
2026-03-02 14:59:43 +0530

    new chnages in web

--- Recon_proto/Controllers/KoSequenceController.cs
+++ Recon_proto/Controllers/KoSequenceController.cs
@@ -577,5 +577,50 @@ namespace Recon_proto.Controllers
             public string? in_user_code { get; set; }
         }
         #endregion
+
+        #region ecfchecklistinfo
+        [HttpPost]
+        public JsonResult ecfchecklistinfo([FromBody] ecfchecklistmodel context)
+        {
+            urlstring = _configuration.GetSection("Appsettings")["apiurl"].ToString();
+            DataTable result = new DataTable();
+            string post_data = "";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string Urlcon = "KoSequence/";
+                    client.BaseAddress = new Uri(urlstring + Urlcon);
+                    //client.BaseAddress = new Uri("http://localhost:4195/api/Recon/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.Timeout = Timeout.InfiniteTimeSpan;
+                    client.DefaultRequestHeaders.Add("user_code", context.in_user_code);
+                    client.DefaultRequestHeaders.Add("lang_code", _configuration.GetSection("AppSettings")["lang_code"].ToString());
+                    client.DefaultRequestHeaders.Add("role_code", _configuration.GetSection("AppSettings")["role_code"].ToString());
+                    client.DefaultRequestHeaders.Add("ipaddress", _configuration.GetSection("AppSettings")["ipaddress"].ToString());
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpContent content = new StringContent(JsonConvert.SerializeObject(context), UTF8Encoding.UTF8, "application/json");
+                    var response = client.PostAsync("ecfchecklistinfo", content).Result;
+                    Stream data = response.Content.ReadAsStreamAsync().Result;
+                    StreamReader reader = new StreamReader(data);
+                    post_data = reader.ReadToEnd();
+                    string d2 = JsonConvert.DeserializeObject<string>(post_data);
+                    return Json(d2);
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonController objcom = new CommonController(_configuration);
+                objcom.errorlog(ex.Message, "ecfchecklistinfo");
+                return Json(ex.Message);
+            }
+        }
+
+        public class ecfchecklistmodel
+        {
+            public string? in_checklist_json { get; set; }
+            public string? in_user_code { get; set; }
+        }
+        #endregion
     }
 }
